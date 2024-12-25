b035288651822c6574110b60c13c19eb575bcb2b
Pavlo Asieiev <726030@student.inholland.nl>
2024-12-25 23:10:40 +0100

    other file

--- main1_0.py
+++ main1_0.py
@@ -10,9 +10,19 @@ def create_slider(window, from_value: int, to_value: int, label: str, resolution
 
     return slider
 
+def update_slider_color(slider, diff, tolerance):
+    if diff < tolerance:
+        slider.configure(troughcolor="green")
+    else:
+        slider.configure(troughcolor="red")
+
+
+
 def start():
     def exit_app(window):
         window.destroy()
+        
+
 
     def centrum_window(window,title: str, width, height):
         screen_width = window.winfo_screenwidth()
@@ -26,22 +36,29 @@ def start():
         window.resizable(False,False)
         return window
 
+
     def restart(window):
         window.destroy()
         start()
+
 
     def draw_tree(x, y, length, angle, canvas, iteration, branch_angle , length_ratio: float ,tag, first_iter = True,):
         if iteration == 0:
             return
         
+        
+
         current_angle = 90 if first_iter else angle
         colour = "brown" if first_iter else "green"
+
+        
 
         x_end = x + length * np.cos(np.radians(current_angle))
         y_end = y - length * np.sin(np.radians(current_angle))
 
         canvas.create_line(x, y, x_end, y_end,width = 3, fill=colour , tags=tag)
-    
+        
+
         draw_tree(x_end, y_end, length * length_ratio, angle + branch_angle, canvas, iteration-1, branch_angle, length_ratio,tag, first_iter= False)
         draw_tree(x_end, y_end, length * length_ratio, angle - branch_angle, canvas, iteration-1, branch_angle, length_ratio,tag, first_iter= False)
 
@@ -50,6 +67,8 @@ def start():
         # Clear the previous tree
         canvas.delete("updated")
         
+        
+
         # Get slider values
         length = length_slider.get()
         angle = angle_slider.get()
@@ -57,8 +76,10 @@ def start():
         branch_angle = branch_angle_slider.get()  # Get angle between branches value
         lengthRatio = length_ratio_slider.get()
         
+        
+        
         # Draw the updated tree
-        draw_tree(800, 500, length, angle, canvas, iterations, branch_angle, lengthRatio, tag="updated", first_iter=True,)
+        draw_tree(800, 600, length, angle, canvas, iterations, branch_angle, lengthRatio, tag="updated", first_iter=True,)
         check_match()
 
     def check_match():
@@ -89,17 +110,16 @@ def start():
         if all(diff[key] <= tolerance[key] for key in tolerance):
             for slider in [length_slider, angle_slider, iteration_slider, branch_angle_slider, length_ratio_slider]:
                 slider.config(state=tk.DISABLED)
-                # slider.config(bg = 'green', fg = 'green ')
             # if 'congrat_win' in globals():
             #     congrat_win.destroy()
             show_congratulations()
         
 
     def show_congratulations():
-        # global congrat_win
         congrat_win = tk.Toplevel(win)
         centrum_window(congrat_win, "Congratulations", 300 , 300)
         congrat_label1 = tk.Label(congrat_win, bg='black')
+
         restart_button = tk.Button(congrat_label1, text="Restart", command= lambda: restart(win))
         congrat_label = tk.Label(congrat_win, text="Congratulations! \n "
                                 'You\'ve matched the random tree!', font=("Arial", 14))
@@ -110,11 +130,14 @@ def start():
         restart_button.pack(padx=10, pady=10)
         exit_button.pack(padx=10 , pady=10)
 
+        
+
+
     def generate_random_tree():
         global random_params
         random_params = {
             'x': 300,
-            'y': 500,
+            'y': 600,
             'length': randint(80, 180),
             'angle': randint(0, 180),
             'canvas': canvas,
@@ -125,28 +148,66 @@ def start():
         print(random_params)
         draw_tree(**random_params, first_iter=True , tag="random" ,)
 
+
+
+   
     win = tk.Tk()
-    win.config(bg='grey')
     width_win = 1200 
-    centrum_window(win, "Matching Game" , width_win, 800)   
+    centrum_window(win, "Matching Game" , width_win, 900)   
     #canvas = tk.Canvas(win, bg='green', width=1000, height=600)
-    canvas = tk.Canvas(win, bg='#e0f7da', width=width_win, height=500)  # Pale green background
+    canvas = tk.Canvas(win, width=width_win-2, height=600)  # Pale green background bg='#e0f7da'
+    # canvas.create_rectangle(0, 0, width_win - 2, 300, fill="#87CEEB")  # Sky blue background
+    # canvas.create_rectangle(0, 600, width_win - 2, 300, fill="#228B22")  # Ground (green)
+
+
 
     ScalesLabel = tk.LabelFrame(win, text="Adjust Tree Parameters", font=("Arial", 14), padx=10, pady=10)
 
     # Create sliders for controlling the tree's parameters
-    length_slider = create_slider(ScalesLabel,50,200,"Initial Branch Length", 1, lambda e: update_tree(),200, 100, 0 , 0)
+    # length_slider = create_slider(ScalesLabel,50,200,"Initial Branch Length", 1, update_tree(length1),200, 100, 0 , 0)
+    length_slider = create_slider(ScalesLabel, 50, 200, "Initial Branch Length", 1, lambda e: update_tree(), 200, 100, 0, 0)
+
     angle_slider = create_slider(ScalesLabel, 0 , 180, "Branch Angle", 1, lambda e: update_tree(), 200, 45, 0,1)
+    # angle_slider = tk.Scale(ScalesLabel, from_=0, to=180, orient=tk.HORIZONTAL, label="Branch Angle", resolution=1, command=lambda e: update_tree() , length=200)
+    # angle_slider.set(45)
+    # angle_slider.grid(row=0, column=1, padx=10, pady=10)
+
     iteration_slider = create_slider(ScalesLabel, 1,10,"Iterations", 1, lambda e: update_tree(), 200, 6,0,2)
+    # iteration_slider = tk.Scale(ScalesLabel, from_=1, to=10, orient=tk.HORIZONTAL, label="Iterations", resolution=1, command=lambda e: update_tree() , length=200)
+    # iteration_slider.set(6)
+    # iteration_slider.grid(row=0, column=2, padx=10, pady=10)
     # Add a slider for controlling the angle between the branches
+
     branch_angle_slider = create_slider(ScalesLabel, 0, 90, "Angle Between Branches", 1, lambda e: update_tree(), 200, 30,0,3)
-    length_ratio_slider = create_slider(ScalesLabel, 0.01,1,'Length Ratio', 0.01, lambda e: update_tree(), 200, 0.7, 0 ,4)
-    
+    # branch_angle_slider = tk.Scale(ScalesLabel, from_=0, to=90, orient=tk.HORIZONTAL, label="Angle Between Branches", resolution=1, command=lambda e: update_tree() , length=200)
+    # branch_angle_slider.set(30)
+    # branch_angle_slider.grid(row=0, column=3, padx=10, pady=10)
+
+
+    length_ratio_slider = create_slider(ScalesLabel, 0.1,1,'Length Ratio', 0.01, lambda e: update_tree(), 200, 0.7, 0 ,4)
+    # length_ratio_slider = tk.Scale(ScalesLabel, from_=0.1, to=1, orient=tk.HORIZONTAL, label="Length Ratio", resolution=0.01, command= lambda e: update_tree(),  length=200)
+    # length_ratio_slider.set(0.7)
+    # length_ratio_slider.grid(row=0, column=4, padx=10, pady=10)
+
+    summer_Photo = tk.PhotoImage(file="/Users/pavloasieiev/Desktop/inholland/term2/python2/python_codes/project_codes/photos_season/winter1png.png" )
+    summer_Photo = summer_Photo.subsample(1)
+    canvas.create_image(600,300, image= summer_Photo)
+    # canvas.create_rectangle(0,550,width_win, 605, fill='#696f6f')
+
     generate_random_tree()
+
     update_tree()
 
-    canvas.pack(padx=10, pady=1)
-    ScalesLabel.pack(padx=1, pady=10)
+
+
+    canvas.pack(padx=1, pady=1)
+    ScalesLabel.pack(padx=10, pady=10)
     win.mainloop()
 
 start()
+
+
+
+
+
+
